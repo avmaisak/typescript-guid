7354bb58ddd196eb0c025da0bd4e317b66108e89
Anton Maisak <anton@maisak.ru>
2020-06-25 19:31:49 +0300

    fix linter warnings (#4)
    updated packages
    Deleted tsconfig
    removed tslint

--- lib/guid.ts
+++ lib/guid.ts
@@ -2,13 +2,13 @@
 export const globals = {
     guidDefaultValue: '00000000-0000-0000-0000-000000000000',
     validationPattern: '^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$'
-}
+};
 
 export class Guid {
 
-    private static gen(count: number) {
-        let out: string = '';
-        for (let i: number = 0; i < count; i++) out += (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    private static gen(count = 1) {
+        let out = '';
+        for (let i = 0; i < count; i++) out += ((1 + Math.random()) * 0x10000 | 0).toString(16).substring(1);
         return out;
     }
 
@@ -17,31 +17,33 @@ export class Guid {
     protected constructor(guid: string) {
         if (!guid) throw new TypeError('Invalid argument; `value` has no value.');
         this.value = globals.guidDefaultValue;
-
         if (guid && Guid.isGuid(guid)) this.value = guid;
     }
 
     static validator = new RegExp(globals.validationPattern, 'i');
 
-    static isGuid = (guid: any) => guid && (guid instanceof Guid || Guid.validator.test(guid.toString()));
+    static isGuid = (guid: Guid | string): boolean => guid && (guid instanceof Guid || Guid.validator.test(guid.toString()));
 
-    static create = (): Guid => new Guid([Guid.gen(2), Guid.gen(1), Guid.gen(1), Guid.gen(1), Guid.gen(3)].join('-'));
+    static create = (): Guid => new Guid([Guid.gen(2), Guid.gen(), Guid.gen(), Guid.gen(), Guid.gen(3)].join('-'));
 
     static createEmpty = (): Guid => new Guid(globals.guidDefaultValue);
 
     static parse = (guid: string): Guid => new Guid(guid);
 
-    static raw = (): string => [Guid.gen(2), Guid.gen(1), Guid.gen(1), Guid.gen(1), Guid.gen(3)].join('-');
+    static raw = (): string => [Guid.gen(2), Guid.gen(), Guid.gen(), Guid.gen(), Guid.gen(3)].join('-');
 
     static EMPTY = Guid.parse(globals.guidDefaultValue);
 
-    // Comparing string `value` against provided `guid` will auto-call
-    // toString on `guid` for comparison
+    /*
+     * Comparing string `value` against provided `guid` will auto-call
+     * toString on `guid` for comparison
+     */
     equals = (other: Guid): boolean => Guid.isGuid(other) && this.value.toUpperCase() === other.toString().toUpperCase();
 
     isEmpty = (): boolean => this.value === globals.guidDefaultValue;
 
     toString = (): string => this.value;
 
-    toJSON(): any { return { value: this.value } }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    toJSON(): any { return { value: this.value }; }
 }
