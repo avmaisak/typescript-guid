5ba1cdb6b3e9b2fbcc8d78128fa910c3aeeac09f
Anton Maisak <anton@maisak.ru>
2020-09-08 09:02:04 +0300

    updated pkg (#6)
    move globals to seperate file

--- lib/guid.ts
+++ lib/guid.ts
@@ -1,8 +1,4 @@
-
-export const globals = {
-    guidDefaultValue: '00000000-0000-0000-0000-000000000000',
-    validationPattern: '^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$'
-};
+import { globals } from "./globals";
 
 export class Guid {
 
